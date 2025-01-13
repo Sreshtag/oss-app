db33074bf011ddac9546768990b76266b59cb2d0
Sreshta Gollapally <sreshtag11@gmail.com>
2025-01-13 08:07:56 +0530

    yesterdays changes

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,15 @@
 import { LightningElement } from 'lwc';
+const defaultData={Name:'',Email:'',EmployeeId:''}
 
-export default class App extends LightningElement {}
+
+export default class App extends LightningElement {
+    formData=defaultData
+    formChange(event){
+        const{name,value} =event.target;
+        this.formData = {...this.formData,[name]:value}
+    }
+    checkinHandler(event){
+        event.preventDefault()
+        console.log(this.formData)
+    }
+}
